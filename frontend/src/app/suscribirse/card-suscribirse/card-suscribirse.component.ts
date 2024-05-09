ac299b58aebf40d3fe4a2654859d6b4049df2dd2
FlorenciaItati <riosflorenciaitati@gmail.com>
2024-05-09 00:37:47 -0300

    modulo y componente suscribirse y cardSuscribirse creados

---
+++ frontend/src/app/suscribirse/card-suscribirse/card-suscribirse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-suscribirse',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-suscribirse.component.html',
+  styleUrl: './card-suscribirse.component.css'
+})
+export class CardSuscribirseComponent {
+
+}
---
+++ frontend/src/app/suscribirse/suscribirse/suscribirse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suscribirse',
+  standalone: true,
+  imports: [],
+  templateUrl: './suscribirse.component.html',
+  styleUrl: './suscribirse.component.css'
+})
+export class SuscribirseComponent {
+
+}
