e1335fd94cbef665f2398e07475140e744456e77
Yanina-Unrein <unrein.syanina@gmail.com>
2024-05-14 00:05:49 -0300

    perfil responsivo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,10 +4,8 @@ import { LoginComponent } from './user/login/login.component';
 import { LayoutComponent } from './user/layout/layout.component';
 import { FeedComponent } from './feed/feed/feed.component';
 import { VistaRecetaComponent } from './receta/vista-receta/vista-receta.component';
-import { SuscribirseComponent } from './suscribirse/suscribirse/suscribirse.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { PerfilComponent } from './user/perfil/perfil.component';
+import { SuscribirseComponent } from './suscribirse/suscribirse/suscribirse.component';
 
 export const routes: Routes = [
     {
@@ -21,6 +19,32 @@ export const routes: Routes = [
     },
     {
         path: 'registrarse',
-        component: RegistrarseComponent      
+        component: RegistrarseComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'feed',
+                component:FeedComponent,
+
+            },
+            {
+                path: 'feed/:id',
+                component: VistaRecetaComponent
+            },
+            {
+                path:'perfil',
+                component:PerfilComponent,
+
+            },
+            {
+                path:'suscribirse',
+                component:SuscribirseComponent,
+
+            },
+            
+        ]
     }
 ];
